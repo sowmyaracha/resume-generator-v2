3ab1d052bdfbee15e29e77fc59e77578cdb34216
Sowmya Racha <sowmyaracha1999@gmail.com>
2026-05-06 16:16:20 -0500

    Remove unused files

--- test_api.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-result = subprocess.run(['grep', '-n', 'process_JD\|process_jd\|requires_citizenship\|profile_relevance', 'langChain.py'], capture_output=True, text=True)
-print(result.stdout)
